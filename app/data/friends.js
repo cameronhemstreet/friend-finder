c3f33abba2172496127361811526b025e311eaf0
Cameron Hemstreet <cameronhemstreet@gmail.com>
2019-08-21 13:49:31 -0700

    ya boi

---
+++ app/data/friends.js
@@ -0,0 +1,133 @@
+var friends = [
+  {
+    name: "Julian",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/0/0e/Julian2.gif/revision/latest?cb=20091104025647",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "2",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jim Lahey",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/f/f5/Lahey.jpg/revision/latest?cb=20091104161132",
+    scores: [
+      "3",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Randy",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/a/a0/Randy.jpg/revision/latest/scale-to-width-down/180?cb=20091104170948",
+    scores: [
+      "5",
+      "2",
+      "4",
+      "2",
+      "4",
+      "2",
+      "3",
+      "2",
+      "1",
+      "5"
+    ]
+  },
+  {
+    name: "Bubbles",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/6/63/Bubbles.jpg/revision/latest?cb=20091104160201",
+    scores: [
+      "3",
+      "2",
+      "4",
+      "4",
+      "2",
+      "1",
+      "5",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Ricky",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/0/00/Ricky.jpg/revision/latest?cb=20091104160712",
+    scores: [
+      "1",
+      "3",
+      "4",
+      "2",
+      "5",
+      "2",
+      "3",
+      "3",
+      "1",
+      "3"
+    ]
+  },
+  {
+    name: "Cory",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/8/8a/Cory.PNG/revision/latest/scale-to-width-down/220?cb=20180328033326",
+    scores: [
+      "1",
+      "4",
+      "2",
+      "2",
+      "5",
+      "2",
+      "4",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name: "J-Roc",
+    photo: "https://i.pinimg.com/564x/0e/1f/4e/0e1f4ed6814724d48197335093fdced7.jpg",
+    scores: [
+      "5",
+      "2",
+      "5",
+      "2",
+      "4",
+      "3",
+      "3",
+      "2",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: "Trevor",
+    photo: "https://vignette.wikia.nocookie.net/trailerpark/images/f/fc/B64e945ca05795fd0debcff6d5b7d9d0--boy-character-trailer-park-boys.jpg/revision/latest?cb=20180328032711",
+    scores: [
+      "5",
+      "1",
+      "2",
+      "3",
+      "2",
+      "1",
+      "3",
+      "2",
+      "3",
+      "5"
+    ]
+  },
+
+];
+
+module.exports = friends;
